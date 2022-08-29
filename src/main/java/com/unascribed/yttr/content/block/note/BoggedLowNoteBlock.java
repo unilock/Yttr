69a2ae8efa7a2ef6d86f7b11301ba35460db703c
Una Thompson <me@unascribed.com>
2022-08-28 19:46:48 -0700

    Fix Bogged Baritone Note Block being a Soprano in disguise

--- src/main/java/com/unascribed/yttr/content/block/note/BoggedLowNoteBlock.java
+++ src/main/java/com/unascribed/yttr/content/block/note/BoggedLowNoteBlock.java
@@ -2,7 +2,7 @@ package com.unascribed.yttr.content.block.note;
 
 import com.unascribed.yttr.mixinsupport.Bogged;
 
-public class BoggedLowNoteBlock extends HighNoteBlock implements Bogged {
+public class BoggedLowNoteBlock extends LowNoteBlock implements Bogged {
 
 	public BoggedLowNoteBlock(Settings settings) {
 		super(settings);
