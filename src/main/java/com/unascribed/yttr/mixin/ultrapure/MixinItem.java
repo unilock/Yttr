b588fd55cb43f1cbedd76b65cd3ed53ad5f2a5a5
Una Thompson <me@unascribed.com>
2022-09-19 21:29:46 -0700

    Fix durability bar for ultrapure tools
    Thanks to Noobulus for bringing this to my attention and sending a patch; I rewrote it to not copy/paste vanilla code with an unconditionally-cancelled inject

---
+++ src/main/java/com/unascribed/yttr/mixin/ultrapure/MixinItem.java
@@ -0,0 +1,37 @@
+package com.unascribed.yttr.mixin.ultrapure;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public class MixinItem {
+
+	@Shadow @Final @Mutable
+	private int maxDamage;
+	
+	private Integer yttr$oldMaxDamage;
+	
+	@Inject(at=@At("HEAD"), method={"getItemBarStep", "getItemBarColor"}, cancellable=true)
+	public void correctMaxDura(ItemStack stack, CallbackInfoReturnable<Integer> ci) {
+		if (stack.hasNbt() && stack.getNbt().contains("yttr:DurabilityBonus")) {
+			yttr$oldMaxDamage = this.maxDamage;
+			this.maxDamage = stack.getMaxDamage();
+		}
+	}
+	
+	@Inject(at=@At("RETURN"), method={"getItemBarStep", "getItemBarColor"}, cancellable=true)
+	public void uncorrectMaxDura(ItemStack stack, CallbackInfoReturnable<Integer> ci) {
+		if (yttr$oldMaxDamage != null) {
+			this.maxDamage = yttr$oldMaxDamage;
+			yttr$oldMaxDamage = null;
+		}
+	}
+	
+}
